7570d323047a5f96d1d44d52264495c56c83ada7
jonschwadron <jonschwadron@gmail.com>
2015-09-07 09:36:08 -0700

    updated functions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,26 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    // if (move == null) {
+    //   return move;
+    // }
+    // else {
+    //   return getInput();
+    // }
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    // if (move == null) {
+    //   return move;
+    // }
+    // else {
+    //   return randomPlay();
+    // }
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -52,4 +64,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
